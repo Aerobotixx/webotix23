f25ffb49bc0cfabcb6e3ab5343b9614fc2db99d3
Shailesh Dinde <shdinde@gmail.com>
2023-08-06 19:14:06 +0530

    Update sent email code

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,81 @@
 <?php
-  
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+require('PHPMailer.php');
+require('SMTP.php');
+require('Exception.php');
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+if(isset($_POST['name'])){
+  $mail = new PHPMailer(true);
+  try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->CharSet = 'UTF-8';
+    $mail->SMTPDebug = 0;                                           //Enable verbose debug output
+    //Send using SMTP
+    $mail->Host       = 'smtp.hostinger.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                       //Enable SMTP authentication
+    $mail->Username   = 'noreply@aerobotix.in';                          //SMTP username
+    $mail->Password   = 'Aerobotix@2023';                               //SMTP password
+    $mail->SMTPSecure = 'ssl';                                      //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    //Recipients
+    $mail->setFrom('noreply@aerobotix.in', 'Aerobotix Tech Solutions');
+    $mail->addAddress('info@aerobotix.in');     //Add a recipient    
+    $mail->addReplyTo('info@aerobotix.in', 'Aerobotix Tech Solutions');   
+   
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = "Contact from ". $_POST['name'];
+    $mail->Body    = 'Subject : '.$_POST['subject'].'<br>'.
+                      'Email : '.$_POST['email'].'<br>'.
+                      'Message : '.$_POST['message'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    if (!$mail->Send()) {
+        echo "Mail is not sent please try again!!!";
+    } else {
+        echo "OK";
+    }
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    //Send thank you to user
+    //Recipients
+    $mail1 = new PHPMailer(true);
+    $mail1->isSMTP();
+    $mail1->CharSet = 'UTF-8';
+    $mail1->SMTPDebug = 0;                                           //Enable verbose debug output
+    //Send using SMTP
+    $mail1->Host       = 'smtp.hostinger.com';                       //Set the SMTP server to send through
+    $mail1->SMTPAuth   = true;                                       //Enable SMTP authentication
+    $mail1->Username   = 'noreply@aerobotix.in';                          //SMTP username
+    $mail1->Password   = 'Aerobotix@2023';                               //SMTP password
+    $mail1->SMTPSecure = 'ssl';                                      //Enable implicit TLS encryption
+    $mail1->Port       = 465;
 
-  echo $contact->send();
-?>
+    $mail1->setFrom('noreply@aerobotix.in', 'Aerobotix Tech Solutions');
+    $mail1->addAddress($_POST['email']);     //Add a recipient    
+    $mail1->addReplyTo('info@aerobotix.in', 'Aerobotix Tech Solutions');   
+   
+    //Content
+    $mail1->isHTML(true);                                  //Set email format to HTML
+    $mail1->Subject = "Thank you for contact Aerobotix Tech Solutions";
+    $mail1->Body    = 'Hello '.$_POST['name'].',<br>'.
+     'Thank you for reaching out to us. We appreciate your interest in our company and are happy to help answer any questions you may have. We will respond to your inquiry as soon as possible. Thank you again for your interest, and we look forward to hearing from you soon. '.
+     '<br><br> Regards <br><br> Aerobotix Tech Solutions';
+    
+    if (!$mail1->Send()) {
+        // echo "Mail is not sent please try again!!!";
+    } else {
+        // echo "OK";
+    }
+
+} catch (Exception $e) {
+    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+}
